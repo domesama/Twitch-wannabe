4165530dc8b164dfa0300d7043562fc11abfb280
Thethawat Kongyu <thethawat.k@mail.kmutt.ac.th>
2020-11-22 23:55:03 +0700

    [Add] Redux Debug implementations

--- src/index.js
+++ src/index.js
@@ -1,13 +1,14 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import { Provider } from "react-redux";
-import { createStore } from "redux";
+import { createStore, applyMiddleware, compose } from "redux";
 
 import App from "./components/App";
 import reducers from "./reducers";
 
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 ReactDOM.render(
-  <Provider store={createStore(reducers)}>
+  <Provider store={createStore(reducers, composeEnhancers(applyMiddleware()))}>
     <App />
   </Provider>,
   document.querySelector("#root")
