cf15a08d0660d1148cec9ba922ffefd1eadd35f8
Thethawat Kongyu <thethawat.k@mail.kmutt.ac.th>
2020-12-17 16:24:53 +0700

    [Add] Action Creators

--- frontend/src/actions/index.js
+++ frontend/src/actions/index.js
@@ -1,5 +1,14 @@
-import { SIGN_IN, SIGN_OUT, CREATE_STREAM } from "./types";
+import {
+  SIGN_IN,
+  SIGN_OUT,
+  CREATE_STREAM,
+  FETCH_STREAMS,
+  FETCH_STREAM,
+  DELETE_STREAM,
+  EDIT_STREAM,
+} from "./types";
 import streams from "../apis/streams";
+
 export const signIn = (userId) => {
   return {
     type: SIGN_IN,
@@ -24,5 +33,36 @@ export const createStreams = (formValues) => {
     const res = await streams.post("/streams", formValues);
 
     dispatch({ type: CREATE_STREAM, payload: res.data });
+  };
+};
+
+export const fetchAllStreams = () => {
+  return async (dispatch) => {
+    const res = await streams.get("/streams");
+    dispatch({ type: FETCH_STREAMS, payload: res.data });
+  };
+};
+
+export const fetchSingleStream = (id) => {
+  return async (dispatch) => {
+    const res = await streams.get(`/streams/${id}`);
+    dispatch({
+      type: FETCH_STREAM,
+      payload: res.data,
+    });
+  };
+};
+
+export const updateStream = (id, content) => {
+  return async (dispatch) => {
+    const res = await streams.put(`/streams/${id}`, content);
+    dispatch({ type: EDIT_STREAM, payload: res.data });
+  };
+};
+
+export const deleteStream = (id) => {
+  return async (dispatch) => {
+    await streams.delete(`/streams/${id}`);
+    dispatch({ type: DELETE_STREAM, payload: id });
   };
 };
