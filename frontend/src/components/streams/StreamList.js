ddb8a31c1c717c7024636034a19e404e475fe496
Thethawat Kongyu <thethawat.k@mail.kmutt.ac.th>
2020-12-18 21:22:47 +0700

    [Add] Cards replacements for segments

--- frontend/src/components/streams/StreamList.js
+++ frontend/src/components/streams/StreamList.js
@@ -8,37 +8,58 @@ function StreamList({ fetchAllStreams, streams }) {
     // console.log(streams);
   }, [fetchAllStreams]);
 
-  const fetchListsOfStreams = () => {
-    const item = [];
+  // const fetchListsOfStreams = () => {
+  //   const item = [];
 
-    for (const stream in streams) {
-      console.log(streams[stream]);
-      item.push(
-        <div className="ui segment" key={streams[stream]["id"]}>
-          <div className="ui header">{streams[stream]["title"]}</div>
-          <div>{streams[stream]["description"]}</div>
+  //   for (const stream in streams) {
+  //     console.log(streams[stream]);
+  //     item.push(
+  //       <div className="ui segment" key={streams[stream]["id"]}>
+  //         <div className="ui header">{streams[stream]["title"]}</div>
+  //         <div>{streams[stream]["description"]}</div>
+  //       </div>
+  //     );
+  //   }
+  //   return item;
+  // };
+
+  const fetchListsOfStreams = () => {
+    return streams.map((stream) => {
+      return (
+        <div className="column" key={stream["id"]}>
+          <div className="ui centered card ">
+            <h2
+              className="ui center aligned icon header"
+              style={{ paddingTop: "25px" }}
+            >
+              <i className="ui icon camera"></i>
+            </h2>
+            <div
+              className="ui center aligned header"
+              style={{ marginTop: "0px" }}
+            >
+              {stream["title"]}
+            </div>
+            <div className="extra" style={{ textAlign: "center" }}>
+              {stream["description"]}
+            </div>
+          </div>
         </div>
       );
-    }
-    return item;
+    });
   };
-  // streams.map((stream) => {
-  //   return (
-  //     <div className="ui segment" key={stream["id"]}>
-  //       <div className="ui header">{stream["title"]}</div>
-  //       <div>{stream["description"]}</div>
-  //     </div>
-  //   );
-  // }
-  // );
 
-  return <div>{fetchListsOfStreams()}</div>;
+  return (
+    <div className="ui three column doubling stackable grid container">
+      {fetchListsOfStreams()}
+    </div>
+  );
 }
 
 const mapStateToProps = (state) => {
   return {
-    // streams: Object.values(state.streams),
-    streams: state.streams,
+    streams: Object.values(state.streams),
+    // streams: state.streams,
   };
 };
 export default connect(mapStateToProps, { fetchAllStreams })(StreamList);
