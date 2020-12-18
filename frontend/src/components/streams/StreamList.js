fea2e391633885738289021dd1130f0117d5a789
Thethawat Kongyu <thethawat.k@mail.kmutt.ac.th>
2020-12-18 16:51:18 +0700

    [Add] Big brain for loops instead of .map()

--- frontend/src/components/streams/StreamList.js
+++ frontend/src/components/streams/StreamList.js
@@ -1,7 +1,44 @@
-import React from "react";
+import React, { useEffect } from "react";
+import { connect } from "react-redux";
+import { fetchAllStreams } from "../../actions";
 
-function StreamList() {
-  return <div>StreamLists</div>;
+function StreamList({ fetchAllStreams, streams }) {
+  useEffect(() => {
+    fetchAllStreams();
+    // console.log(streams);
+  }, [fetchAllStreams]);
+
+  const fetchListsOfStreams = () => {
+    const item = [];
+
+    for (const stream in streams) {
+      console.log(streams[stream]);
+      item.push(
+        <div className="ui segment" key={streams[stream]["id"]}>
+          <div className="ui header">{streams[stream]["title"]}</div>
+          <div>{streams[stream]["description"]}</div>
+        </div>
+      );
+    }
+    return item;
+  };
+  // streams.map((stream) => {
+  //   return (
+  //     <div className="ui segment" key={stream["id"]}>
+  //       <div className="ui header">{stream["title"]}</div>
+  //       <div>{stream["description"]}</div>
+  //     </div>
+  //   );
+  // }
+  // );
+
+  return <div>{fetchListsOfStreams()}</div>;
 }
 
-export default StreamList;
+const mapStateToProps = (state) => {
+  return {
+    // streams: Object.values(state.streams),
+    streams: state.streams,
+  };
+};
+export default connect(mapStateToProps, { fetchAllStreams })(StreamList);
--- frontend/src/reducers/index.js
+++ frontend/src/reducers/index.js
@@ -1,9 +1,11 @@
 import { combineReducers } from "redux";
 import { reducer as formReducer } from "redux-form";
 
+import { streamReducer } from "./streamReducer";
 import { authReducer } from "./authReducer";
 
 export default combineReducers({
   auth: authReducer,
   form: formReducer,
+  streams: streamReducer,
 });
