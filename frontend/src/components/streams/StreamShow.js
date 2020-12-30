19dee2238a9a06317aa3c08f9eecc60e846c673d
Thethawat Kongyu <kumasamath@gmail.com>
2020-12-30 20:13:28 +0700

    [Add] useRef() null handles using lamda functions

--- frontend/src/components/streams/StreamShow.js
+++ frontend/src/components/streams/StreamShow.js
@@ -4,35 +4,38 @@ import { fetchSingleStream } from "../../actions";
 import flv from "flv.js";
 
 function StreamShow({ fetchSingleStream, match, stream }) {
-  const [hasStream, setHasStream] = useState(false);
   const videoRef = useRef(null);
+  const [isRefNull, setIsRefNull] = useState(null);
 
   useEffect(() => {
-    var player = null;
     fetchSingleStream(match.params.id);
 
     const buildPlayer = () => {
-      if (hasStream || player != null) {
-        return;
-      }
-      setHasStream(true);
-      player = flv.createPlayer({
+      // if (player != null) {
+      //   return;
+      // }
+      const player = flv.createPlayer({
         type: "flv",
         url: `http://localhost:8000/live/${match.params.id}.flv`,
       });
-      player.attachMediaElement(videoRef.current);
-      player.load();
+      if (isRefNull) {
+        player.attachMediaElement(videoRef.current);
+        player.load();
+      }
     };
     buildPlayer();
-    // return () => buildPlayer();
-  }, [fetchSingleStream, match, hasStream]);
+    return () => buildPlayer();
+  }, [fetchSingleStream, match, isRefNull]);
 
   return !stream ? (
     <div> Loading</div>
   ) : (
     <div>
       <video
-        ref={videoRef}
+        ref={(el) => {
+          videoRef.current = el;
+          setIsRefNull(!!el);
+        }}
         style={{ width: "100%", height: "40%" }}
         controls={true}
         src=""
